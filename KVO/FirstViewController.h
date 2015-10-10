49c9279d6951d240bd9a0e8d9e8c63c021563d31
Jay Versluis <jay@versluis.com>
2015-10-09 22:50:33 -0400

    Initial Commit

---
+++ KVO/FirstViewController.h
@@ -0,0 +1,15 @@
+//
+//  FirstViewController.h
+//  KVO
+//
+//  Created by Jay Versluis on 09/10/2015.
+//  Copyright © 2015 Pinkstone Pictures LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+
+@end
+
---
+++ KVO/FirstViewController.m
@@ -0,0 +1,27 @@
+//
+//  FirstViewController.m
+//  KVO
+//
+//  Created by Jay Versluis on 09/10/2015.
+//  Copyright © 2015 Pinkstone Pictures LLC. All rights reserved.
+//
+
+#import "FirstViewController.h"
+
+@interface FirstViewController ()
+
+@end
+
+@implementation FirstViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
